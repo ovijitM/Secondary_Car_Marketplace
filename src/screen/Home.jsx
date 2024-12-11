898deb0247e1fb5fdadc0422f18a9d6310729cda
ovijitM <sdsufol@gmail.com>
2024-12-11 17:42:43 +0000

    homepage sections are visible

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -4,20 +4,19 @@ import Carousel from "../components/Carousels";
 import Cards from "../components/Cards";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { useState } from "react";
-import "./Home.css"; // Import custom CSS for the sliding effect
-
+import "./Home.css"; 
 function Home() {
   const [currentSection, setCurrentSection] = useState(0);
 
-  // Section titles
+  
   const sections = ['Cars', 'Rent', 'Repair'];
 
-  // Move to the next section
+ 
   const nextSection = () => {
     setCurrentSection((prevSection) => (prevSection + 1) % sections.length);
   };
 
-  // Move to the previous section
+ 
   const prevSection = () => {
     setCurrentSection((prevSection) => (prevSection - 1 + sections.length) % sections.length);
   };
@@ -26,12 +25,12 @@ function Home() {
     <>
       <Customnavbar />
 
-      {/* Carousel Section */}
+  
       <div className="vh-100">
         <Carousel />
       </div>
 
-      {/* Section Navigation Buttons */}
+
       <div className="d-flex justify-content-between mb-4">
         <Button variant="secondary" onClick={prevSection}>
           Previous
@@ -41,7 +40,7 @@ function Home() {
         </Button>
       </div>
 
-      {/* Main Content Sections */}
+
       <Container className="my-4">
         <div className={`slider-container`}>
           <div >
