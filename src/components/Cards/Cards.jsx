403fbba2735fef27b705a728a4153c2c08d0d2df
naimur <naimurislam707@gmail.com>
2024-12-13 19:15:33 +0600

    modified cars from homescreen

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Home from "../components/Customnavbar/Customnavbar";
+import Home from "../Customnavbar/Customnavbar";
 
 function Cards() {
   return (
--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -3,7 +3,7 @@ import Customnavbar from "../components/Customnavbar/Customnavbar";
 import Carousel_main from "../components/Crousels/Home_main";
 import Carbrand from "../components/Carbrandlogo/Carbrand";
 
-import Cards from "../components/Cards";
+import Cards from "../components/Cards/Cards";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { useState } from "react";
 import "./Home.css";
@@ -30,27 +30,77 @@ function Home() {
         <Carousel_main />
       </div>
 
-      <div>
-        <text>Your Dream Brand</text>
+      <div
+        className="carbrandlogo"
+        style={{ marginTop: "-10px", marginBottom: "50px" }}
+      >
+        <h2>We are Connected with</h2>
 
         <Carbrand />
       </div>
 
       <Container className="my-4">
         <div className={`slider-container`}>
-          <div>
-            <h2>Cars</h2>
-            <Row>
-              <Col style={{ color: "black" }}>
-                <Cards />
-              </Col>
-              <Col style={{ color: "black" }}>
-                <Cards />
-              </Col>
-              <Col style={{ color: "black" }}>
-                <Cards />
-              </Col>
-            </Row>
+          <div className="cars" style={{ margin: "5px 10px 50px 10px" }}>
+            <h2>Choose Your Dream Car</h2>
+            <div className="choose-cars">
+              <div className="caritem iem1">
+                <Cards
+                  title="Card 3"
+                  description="This is the third card"
+                  image="https://via.placeholder.com/150"
+                />
+              </div>
+              <div className="caritem iem2">
+                <Cards
+                  title="Card 3"
+                  description="This is the third card"
+                  image="https://via.placeholder.com/150"
+                />
+              </div>
+              <div className="caritem iem3">
+                <Cards
+                  title="Card 3"
+                  description="This is the third card"
+                  image="https://via.placeholder.com/150"
+                />
+              </div>
+              <div className="caritem iem4">
+                <Cards
+                  title="Card 3"
+                  description="This is the third card"
+                  image="https://via.placeholder.com/150"
+                />
+              </div>
+              <div className="caritem iem5">
+                <Cards
+                  title="Card 3"
+                  description="This is the third card"
+                  image="https://via.placeholder.com/150"
+                />
+              </div>
+              <div className="caritem iem6">
+                <Cards
+                  title="Card 3"
+                  description="This is the third card"
+                  image="https://via.placeholder.com/150"
+                />
+              </div>
+              <div className="caritem iem7">
+                <Cards
+                  title="Card 3"
+                  description="This is the third card"
+                  image="https://via.placeholder.com/150"
+                />
+              </div>
+              <div className="caritem iem8">
+                <Cards
+                  title="Card 3"
+                  description="This is the third card"
+                  image="https://via.placeholder.com/150"
+                />
+              </div>
+            </div>
           </div>
 
           <div>
