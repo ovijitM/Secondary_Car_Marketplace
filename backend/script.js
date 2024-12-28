75c6b6dea969df4166a34f22e76314653889ef4c
mdrakibhossain719 <141365102+mdrakibhossain719@users.noreply.github.com>
2024-12-29 04:17:41 +0600

    5

--- backend/script.js
+++ backend/script.js
@@ -1,22 +1,15 @@
 import Express from "express";
 
-
-
-import userRoutes from './createuser/createuser.js';
-import checkUser from './validateuser/logincheck.js';
-import search from './Search/search.js';
-import logout from './validateuser/logoutcheck.js';
+import userRoutes from "./createuser/createuser.js";
+import checkUser from "./validateuser/logincheck.js";
+import search from "./Search/search.js";
+import logout from "./validateuser/logoutcheck.js";
 import filter from "./Search/filtercar.js";
-import displaydata from './displaydata.js';
-import user_history from './userhistory.js'; // Add this line to import user_history
-
-
-
-
+import displaydata from "./displaydata.js";
+import user_history from "./userhistory.js";
 import rentCar from "./Rent_cars/rent.js";
 import book from "./Booking_car/book_c.js";
 import p from "./Rent_cars/slip.js";
-
 
 const port = 8000;
 const app = Express();
@@ -29,28 +22,21 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
-app.use('/api', userRoutes); 
-app.use('/api', checkUser);
-app.use('/api', search);
-app.use('/api', logout);
-app.use('/api', filter);
-app.use('/api', displaydata);
-app.use('/api', user_history); 
+app.use("/api", userRoutes);
+app.use("/api", checkUser);
+app.use("/api", search);
+app.use("/api", logout);
+app.use("/api", filter);
+app.use("/api", displaydata);
+app.use("/api", user_history);
 app.use("/api", rentCar);
 app.use("/api", book);
 app.use("/api", p);
-
 
 app.use((req, res) => {
   res.send("Hello World!");
 });
 
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
-
